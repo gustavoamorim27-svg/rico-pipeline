c078948f56be9e06d562dfd960bc5f5cfae02850
gustavoamorim27-svg <gustavo.amorim27@gmail.com>
2026-09-25 22:12:02 -0300

    v5.4 (5/5): novo cache para o iPad baixar a versão

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rico-clientes-shell-v3-20260922-4';
+const CACHE='rico-clientes-shell-v3-20260925-1';
 const SHELL=['./','./index.html','./styles.css','./app.mjs','./core.mjs','./store.mjs','./logo.mjs','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('rico-clientes-shell-')&&k!==CACHE).map(k=>caches.delete(k)))));});
